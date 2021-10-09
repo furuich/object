699808e6f26cb07e94754468e3b01fe610996f91
furuich <rock-st39@yahoo.ne.jp>
2021-10-09 21:28:06 +0900

    aa

--- src/ex/ExSample.java
+++ src/ex/ExSample.java
@@ -2,8 +2,6 @@ package ex;
 
 public class ExSample {
     public static void main(String[] args) {
-    Ex ex1= new Ex(1,"aisiteru");//インスタンスを生成して中身を渡してる
-    Ex ex2= new Ex(5,"arigatou");/
 
 
     }
